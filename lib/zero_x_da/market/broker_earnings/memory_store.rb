1cdb3d3276b5f3a89654087a3a6196a02bdc7db9
Sasha Lohozinskyy <0x0sky@proton.me>
2026-08-07 15:10:17 +0300

    feat(payouts): support self-service payout state in memory

--- lib/zero_x_da/market/broker_earnings/memory_store.rb
+++ lib/zero_x_da/market/broker_earnings/memory_store.rb
@@ -2,6 +2,7 @@
 
 require "thread"
 require_relative "../core/contracts"
+require_relative "payout_records"
 
 module ZeroXDA
   module Market
@@ -10,20 +11,14 @@ module ZeroXDA
         def initialize
           @mutex = Mutex.new
           @earnings = {}
+          @profiles = {}
+          @payouts = {}
         end
 
-        def transaction
-          @mutex.synchronize { yield self }
-        end
-
-        def find_by_order(order_id)
-          @earnings.values.find { |earning| earning.order_id == order_id.to_s }
-        end
-
-        def list_by_seller(seller_user_id)
-          @earnings.values.select { |earning| earning.seller_user_id == seller_user_id.to_s }
-                   .sort_by { |earning| [earning.updated_at, earning.id] }.reverse
-        end
+        def transaction = @mutex.synchronize { yield self }
+        def find_by_order(order_id) = @earnings.values.find { |earning| earning.order_id == order_id.to_s }
+        def list_by_seller(seller_user_id) = @earnings.values.select { |earning| earning.seller_user_id == seller_user_id.to_s }.sort_by { |earning| [earning.updated_at, earning.id] }.reverse
+        def available_by_seller(seller_user_id) = list_by_seller(seller_user_id).select { |earning| earning.state == "available" }.reverse
 
         def insert(earning)
           existing = find_by_order(earning.order_id)
@@ -36,6 +31,31 @@ module ZeroXDA
           current = @earnings[earning.id] || raise(Core::NotFound.new("broker_earning", earning.id))
           raise Core::ConcurrencyConflict.new("broker_earning", earning.id) unless current.version == expected_version
           @earnings[earning.id] = earning
+        end
+
+        def payout_profile(seller_user_id) = @profiles[seller_user_id.to_s]
+
+        def upsert_payout_profile(profile, expected_version: nil)
+          current = @profiles[profile.seller_user_id]
+          if current && !expected_version.nil? && current.version != expected_version
+            raise Core::ConcurrencyConflict.new("broker_payout_profile", profile.seller_user_id)
+          end
+          @profiles[profile.seller_user_id] = profile
+        end
+
+        def insert_payout(payout)
+          existing = @payouts.values.find { |item| item.idempotency_key == payout.idempotency_key }
+          return existing if existing
+          @payouts[payout.id] = payout
+        end
+
+        def find_payout(id) = @payouts[id.to_s]
+        def list_payouts(seller_user_id) = @payouts.values.select { |item| item.seller_user_id == seller_user_id.to_s }.sort_by(&:created_at).reverse
+
+        def replace_payout(payout, expected_version:)
+          current = @payouts[payout.id] || raise(Core::NotFound.new("broker_payout", payout.id))
+          raise Core::ConcurrencyConflict.new("broker_payout", payout.id) unless current.version == expected_version
+          @payouts[payout.id] = payout
         end
       end
     end
